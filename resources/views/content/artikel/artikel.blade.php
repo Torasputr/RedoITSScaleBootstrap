4f64ced03bdd54727b4b03ffa9b697bfaf082841
Torasputr <torasangputra@gmail.com>
2024-03-18 17:09:35 +0700

    Tambah tipis-tipis

--- resources/views/content/artikel/artikel.blade.php
+++ resources/views/content/artikel/artikel.blade.php
@@ -2,15 +2,15 @@
 @section('title', 'ITS Scale - Artikel')
 @section('content')
     <div class="artikel-banner d-flex align-items-center justify-content-center">
-        <h1 class='subtitle'>Artikel</h1>
+        <h1 class='subtitle fw-bold text-3xl'>Artikel</h1>
     </div>
     <div class='grid-cols-3 py-5 mx-5'>
         @foreach($articles as $article)
         <a href="/artikel/{{ $article->alt }}">
             <div class='category-card p-3 rounded'>
                 <img class='category-image rounded' src="{{ asset($article->thumbnail) }}" alt="">
-                <h1 class='text-2xl pt-2 category-name fw-'>{{ $article->title }}</h1>
-                <h1 class='category-name text-sm text-end'>{{ $article->date }}</h1>
+                <h1 class='text-lg pt-2 category-name text-truncate'>{{ $article->title }}</h1>
+                <h1 class='category-name text-sm text-end fw-light'>{{ $article->date }}</h1>
             </div>
         </a>
         @endforeach
